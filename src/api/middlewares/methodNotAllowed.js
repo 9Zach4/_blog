b42311732d2900a3f02b8ae3954f071ec853dffb
Zach <djessitokoto@gmail.com>
2024-02-12 15:40:16 +0100

    chore: back to 0

--- src/api/middlewares/methodNotAllowed.js
+++
@@ -1,9 +0,0 @@
-import { HTTP_ERRORS } from "@/api/constants"
-
-const methodNotAllowed = (req, res) => {
-  res
-    .status(HTTP_ERRORS.METHOD_NOT_ALLOWED)
-    .send({ error: "Method not allowed" })
-}
-
-export default methodNotAllowed
--- src/utils/formatters.js
+++
@@ -1,4 +0,0 @@
-export const formatDateTimeShort = new Intl.DateTimeFormat("en-EN", {
-  dateStyle: "short",
-  timeStyle: "short",
-}).format
