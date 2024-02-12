581a94d851e43aa5b53cc6ced846339e1fa588f4
Zach <djessitokoto@gmail.com>
2024-02-12 23:30:42 +0100

    chore: added knex

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,6 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-
 
 export default function Home() {
   return (
-    <p>hello blog</p>
+    <p className="text-lg">hello blog</p>
   ) 
 }
