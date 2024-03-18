ba1a3817ea70e5df3ac921345538f15215d17c04
Zach <djessitokoto@gmail.com>
2024-03-18 02:07:38 +0100

    chore: added comments model's seed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,19 +3,19 @@ import React from "react"
 
 const IndexPage = () => (
   <div className="">
-    <h1 className="text-2xl">
+    <h1 className="text-2xl my-3">
       <a href= "https://www.youtube.com/watch?v=uczY1We-Eak" target="_blank" rel="noopener noreferrer"> Ugly Front-Page</a>
-     
     </h1>
     <p>
       This is the front-page of the blog. 👾  
     </p>
     <p>
-      you can sign-in, Post and see the list of posts and even delete you're own post. 💻
+      you can sign-in, post, comments,  and see the list of posts and even delete you're own post. 💻
     </p>
     <p>
       That's all. Simple. 🤷‍♂️
     </p>
+    <p> I know the blog looks 💩 right now but it will be upgraded in the futur, trust me 🙏🏿</p>
     <p>
       Also, there is an <span className="text-red-600 font-bold">Admin</span>. Who knows what he can do... 👀
     </p>
---
+++ src/pages/post/comments.jsx
@@ -0,0 +1,12 @@
+import useState from "react"
+import apiClient from "@/web/services/apiClient"
+
+
+
+
+const commentPage = () => {
+ 
+}
+
+
+export default commentPage
